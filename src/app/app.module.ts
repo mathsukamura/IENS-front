65bc1eba8cab3f03bfec323987f4cbf2e49985c9
mathsukamura <matheus.yuri.melo@gmail.com>
2023-02-23 14:13:59 -0300

    creation project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/home.component';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     AppComponent
   ],
   imports: [
